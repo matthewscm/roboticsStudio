49601ebe673d4eb9480d4ee5ded4b904d2c6db53
matthewscm <claire.m.matthews@student.uts.edu.au>
2024-09-04 05:39:16 +0000

    update 2

--- lab_4/turtlebot3_state_estimation/src/dead_reckoning.h
+++ lab_4/turtlebot3_state_estimation/src/dead_reckoning.h
@@ -1,26 +1,23 @@
 #ifndef DEAD_RECKONING_H
 #define DEAD_RECKONING_H
 
-#include "rclcpp/rclcpp.hpp"
-#include "geometry_msgs/msg/twist.hpp"
-#include "nav_msgs/msg/odometry.hpp"
-#include <random>
-#include <cmath>
-#ifndef DEAD_RECKONING_H
-#define DEAD_RECKONING_H
-
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/twist.hpp>
 #include <nav_msgs/msg/odometry.hpp>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <random>
 
 class DeadReckoningNode : public rclcpp::Node {
 public:
     DeadReckoningNode();
+    // Functions
+    void move_robot(double linear_speed, double angular_speed);
+    void publish_cmd_vel(double linear_speed, double angular_speed);
+    void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
+    void timer_callback();
 
 private:
-    // State variables
+ // State variables
     double x_;
     double y_;
     double theta_;
@@ -28,6 +25,7 @@ private:
     double start_y_;
     double distance_;
     bool forward_;
+
     
     // Parameters
     double linear_speed_;
@@ -45,11 +43,6 @@ private:
     // Timer
     rclcpp::TimerBase::SharedPtr timer_;
 
-    // Functions
-    void move_robot(double linear_speed, double angular_speed);
-    void publish_cmd_vel(double linear_speed, double angular_speed);
-    void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
-    void timer_callback();
 
     // Helper function to check if the target distance has been reached
     bool has_reached_distance(double tolerance);
